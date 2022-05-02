b1ca8cb6cf76ac43a677140ea0b4c13a14bda983
MNandor <mnandor@users.noreply.github.com>
2022-05-02 12:18:32 +0300

    Check for previous installs in quickinstall

--- quickinstall.sh
+++ quickinstall.sh
@@ -2,6 +2,10 @@
 
 cat .bashrc > ~/.nbashrc
 cat .vimrc > ~/.nvimrc
-echo "so ~/.nvimrc" >> ~/.vimrc
-echo "source ~/.nbashrc" >> ~/.bashrc
+if [[ -z $(grep "nbashrc" ~/.bashrc) ]]; then
+	echo "source ~/.nbashrc" >> ~/.bashrc
+fi
+if [[ -z $(grep "nvimrc" ~/.vimrc) ]]; then
+	echo "so ~/.nvimrc" >> ~/.vimrc
+fi
 source ~/.bashrc
